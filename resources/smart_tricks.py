651e07d14c8536bef51db68972561cb9d97823b5
Pritam Kalbhor <physics.pritam@cern.ch>
2022-04-12 00:17:18 +0200

    Some smart tricks to make code clean

---
+++ resources/smart_tricks.py
@@ -0,0 +1,25 @@
+from requests import Session
+from urllib.parse import urljoin
+
+# Use requests module ignoring baseUrl 
+# https://github.com/psf/requests/issues/2554#issuecomment-109341010
+class LiveServerSession(Session):
+    def __init__(self, prefix_url):
+        self.prefix_url = prefix_url
+        super(LiveServerSession, self).__init__()
+
+    def request(self, method, url, *args, **kwargs):
+        url = urljoin(self.prefix_url, url)
+        return super(LiveServerSession, self).request(method, url, *args, **kwargs)
+askfor = LiveServerSession('http://localhost:5000')
+
+# Use dictionary as object 
+# https://joelmccune.com/python-dictionary-as-object/
+class DictObj:
+    def __init__(self, in_dict:dict):
+        assert isinstance(in_dict, dict)
+        for key, val in in_dict.items():
+            if isinstance(val, (list, tuple)):
+               setattr(self, key, [DictObj(x) if isinstance(x, dict) else x for x in val])
+            else:
+               setattr(self, key, DictObj(val) if isinstance(val, dict) else val)
