9bcb5d0904e771f38472caa4bfb6c32a44845b2d
Pritam Kalbhor <physics.pritam@cern.ch>
2022-04-14 09:53:01 +0200

    styles to avoid word wrapping in table

--- application/tickets/Table.py
+++ application/tickets/Table.py
@@ -2,13 +2,18 @@ from flask import url_for
 from flask_table import Table, Col, LinkCol
 
 
-class LangCol(Col):
+class ActionCol(Col):
     def td_format(self, content):
         edit = f"<a href='/tickets/edit?prepid={content}'>Edit</a>"
         clone = f"<a href='/tickets/edit?clone={content}'>Clone</a>"
         matrix = f"<a href='api/tickets/run_the_matrix/{content}'>runTheMatrix.py</a>"
         output = " | ".join([edit, clone, matrix])
         return output
+
+class WFCol(Col):
+    def td_format(self, content):
+        workflows = ', '.join([str(a) for a in content])
+        return f"<div class='bg-light'>{workflows}</div>"
 
 class ItemTable(Table):
     prepid = LinkCol('Prep ID', endpoint='tickets.tickets', 
@@ -17,15 +22,15 @@ class ItemTable(Table):
 
     status = Col('Status')
 
-    _id = LangCol("Actions", td_html_attrs={'style': 'white-space: nowrap'})
+    _id = ActionCol("Actions", td_html_attrs={'style': 'white-space: nowrap'})
 
     cmssw_release = LinkCol('CMSSW Release', endpoint='tickets.tickets', 
                     url_kwargs=dict(cmssw_release='cmssw_release'), 
-                    anchor_attrs={'class': 'myclass'}, attr='cmssw_release')
+                    anchor_attrs={}, attr='cmssw_release')
 
     batch_name = LinkCol('Batch Name', endpoint='tickets.tickets', 
                     url_kwargs=dict(batch_name='batch_name'), 
-                    anchor_attrs={'class': 'myclass'}, 
+                    anchor_attrs={}, 
                     attr='batch_name'
                 )
 
@@ -37,7 +42,7 @@ class ItemTable(Table):
 
     scram_arch = Col('Scram Arch')
 
-    workflow_ids = Col('Workflows')
+    workflow_ids = WFCol('Workflows', td_html_attrs={'style': 'white-space: nowrap'})
 
     allow_sort = False
     
