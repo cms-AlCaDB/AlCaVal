82608a491dee4e49ead5b7b6d0e6f134c2bc9d57
Pritam Kalbhor <physics.pritam@cern.ch>
2022-04-17 22:54:20 +0200

    Notes and PrepID field added to the FORM

--- application/tickets/forms.py
+++ application/tickets/forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, StringField, FieldList
+from wtforms import SubmitField, SelectField, StringField, FieldList, TextAreaField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
 
 
 class TicketForm(FlaskForm):
+    prepid = StringField('Prep ID')
     batch_name = StringField('Batch Name', validators=[DataRequired()])
     cmssw_release = StringField('CMSSW Release', validators=[DataRequired()])
     label = StringField('Label (--label)')
     hlt_gt = StringField('HLT Global Tag')
     prompt_gt = StringField('Prompt Global Tag')
     express_gt = StringField('Express Global Tag')
+    notes = TextAreaField('Notes',  render_kw={"rows": 10, 
+                          'placeholder': "Notes: e.g. Description of the request. Twiki reference or Jira ticket ID"})
     workflow_ids = StringField('Workflow IDs', validators=[DataRequired()])
     submit = SubmitField('Submit')
