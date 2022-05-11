bf4f740311abb520a048881c8aa16191cd2979fd
Pritam Kalbhor <physics.pritam@cern.ch>
2022-05-11 21:15:28 +0200

    Ticket form edits: Input required for common GT, autoHide common GT

--- application/tickets/forms.py
+++ application/tickets/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField, StringField, FieldList, TextAreaField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import DataRequired, InputRequired, ValidationError, StopValidation, Length
 from wtforms.widgets import TextArea
 
 from markupsafe import Markup
@@ -28,9 +28,33 @@ class CustomSelect:
         html.append("</select>")
         return Markup("".join(html))
 
+class GTDataRequired(object):
+    """Validator for Common Prompt GT
+    """
+    def __init__(self, message=None):
+        self.message = message
+        self.field_flags = {}
+
+    def __call__(self, form, field):
+        hltgt = bool(form.data['hlt_gt'].strip()=='')
+        if field.data and (not isinstance(field.data, str) or field.data.strip()) and not hltgt:
+            return
+        if not (field.data and (not isinstance(field.data, str) or field.data.strip())) and hltgt:
+            return
+
+        if self.message is None:
+            message = field.gettext("This field is required.")
+        else:
+            message = self.message
+
+        field.errors[:] = []
+        raise StopValidation(message)
+
 def validate_jira_ticket(form, field):
-  print(form.data, "FOMR DATA")
-  raise ValidationError("Select options for the Jira ticket")
+    print(form.data, "FOMR DATA")
+    if form.data['hlt_gt'].strip() == '':
+        message = field.gettext('This field is required.')
+        raise StopValidation(message)
 
 class TicketForm(FlaskForm):
     prepid = StringField('Prep ID')
@@ -52,21 +76,14 @@ class TicketForm(FlaskForm):
                            validators=[InputRequired()],
                            default='alca'
                         )
-    hlt_gt = StringField('HLT Global Tag')
+    hlt_gt = StringField('HLT Global Tag', validators=[Length(min=5, max=100)])
     prompt_gt = StringField('Prompt Global Tag')
     express_gt = StringField('Express Global Tag')
-    common_prompt_gt = StringField('Common Prompt Global Tag', 
+    common_prompt_gt = StringField('Common Prompt GT',
+                        validators=[GTDataRequired(message="Since you have chosen to use HLT global tag, you are required to provide common prompt global tag, which is to be used in RECO step of workflow")],
                         render_kw={'placeholder': 'Global tag to be used in RECO step of HLT workflow'}
                         )
     notes = TextAreaField('Notes',  render_kw={"rows": 10, 
                           'placeholder': "Notes: e.g. Description of the request. "})
     workflow_ids = StringField('Workflow IDs', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
-class ValidationError(Exception):
-  def __init__(self, message):                                                 
-      self.message = message                  
-      super().__init__(self.message)                                          
-                                                                              
-  def __str__(self):                                                          
-      return self.message 
