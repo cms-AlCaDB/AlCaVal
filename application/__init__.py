515fd82108337ef39eac670e9cef07fd3fe1ee2a
Pritam Kalbhor <physics.pritam@cern.ch>
2022-06-20 19:18:30 +0200

    Disable debug mode in production instance

--- application/__init__.py
+++ application/__init__.py
@@ -178,6 +178,7 @@ def create_app():
 	Database.add_search_rename('relvals', 'workflow', 'workflows.name')
 	Database.add_search_rename('relvals', 'output_dataset', 'output_datasets')
 
-	logger = setup_logging(True)
+	debug = config.get('development', False)
+	logger = setup_logging(debug)
 	logger.info('Starting... Debug: ')
 	return app
