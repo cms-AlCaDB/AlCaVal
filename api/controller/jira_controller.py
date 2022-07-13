1abee88d329f8c3b1ead260d494024c1ac6031d1
Pritam Kalbhor <physics.pritam@cern.ch>
2022-07-13 22:13:18 +0200

    Endpoint for creating jira ticket

--- api/controller/jira_controller.py
+++ api/controller/jira_controller.py
@@ -10,14 +10,30 @@ class JiraTicketController():
 
     def __init__(self):
         self.database_name = 'tickets'
+        jira = Jira(Config.get('jira_credentials_file'))
+        self.conn = jira.setup_jira_connection()
 
     def get(self):
-        jira = Jira(Config.get('jira_credentials_file'))
-        jira.setup_jira_connection()
         # Summaries of my last 50 reported issues
-        issues = jira.client.search_issues('project=CMSALCA order by created desc', maxResults=50)
+        issues = self.conn.search_issues('project=CMSALCA order by created desc', maxResults=50)
         return issues
 
+    def create_ticket(self, jira_json):
+        fields={
+                'project': 'CMSALCA',
+                'issuetype': {'name': 'Task'},
+                'summary': jira_json['jira_summary'],
+                'description': jira_json['jira_description'],
+                'assignee': {'name': 'tvami'},
+                'priority': {'name': 'Major'},
+                'components': [{'name' : 'AlCaDB'}]
+               }
+        new_issue = self.conn.create_issue(fields = fields)
+        return new_issue.key
+
+    def add_comment(self, text):
+        issue = self.conn.issue('CMSALCA-{}'.format(self.args['Jira']))
+        comment = self.conn.add_comment(issue.key, text)
 
 class Jira():
     def __init__(self, credentials_file, host='http://its.cern.ch/jira'):
@@ -36,3 +52,4 @@ class Jira():
                                         credentials['password']), 
                             options=options)
         self.logger.debug('Done setting up jira connection')
+        return self.client
